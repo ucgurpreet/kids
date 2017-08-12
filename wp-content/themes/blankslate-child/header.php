fa6d1a2668626b0b743e248ea1d274fb10eaa49d
Gurpreet Rai <gurpreetrai94@gmail.com>
2017-08-12 17:50:29 +0530

    make 4 page

--- wp-content/themes/blankslate-child/header.php
+++ wp-content/themes/blankslate-child/header.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
-<link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet"> 
 <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/css/font-awesome.min.css" />
 <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
 <?php wp_head(); ?>
@@ -14,12 +14,12 @@
 	<div class="header">
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-5">
+				<div class="col-sm-3">
 					<div class="logo">
 						<a href="#"><img src="<?php echo get_stylesheet_directory_uri();?>/images/logo.png"></a>
 					</div>
 				</div>
-				<div class="col-sm-7 ">
+				<div class="col-sm-9 ">
 					<div class="top-right-sec">
 						<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
 					</div>
